4b7781ca8d4baef9e56b3445f72b15c35cfd80a9
lfeist <laurent.feisthauer@gmail.com>
2023-10-22 20:13:34 +0200

    Add a log

--- resources/user.py
+++ resources/user.py
@@ -19,11 +19,13 @@ blp = Blueprint("Users", "users", description="Operations on users")
 def send_simple_message(to, subject, body):
   domain = os.getenv("MAILGUN_DOMAIN")
   api_key = os.getenv("MAILGUN_API_KEY")
+
+  print(f"sending a mail to {to}")
   
   return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", api_key),
-		data={"from": f"Excited User <mailgun@{domain}>",
+		data={"from": f"Poussinet <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
 			"text": body})
@@ -51,7 +53,7 @@ class UserRegister(MethodView):
 
     send_simple_message(
       to = user.email,
-      subject = "Hell-o",
+      subject = "Hello {user.username}",
       body = f"Hi {user.username}! You have succesfully signed up to this RestAPI"
     )
 
